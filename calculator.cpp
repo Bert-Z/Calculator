cd20926bf4319f8ca468764ad8b10fbac11a309b
Bert-Z <wanqiangzhang@sjtu.edu.cn>
2018-03-23 15:15:38 +0800

    complete the Token and Token_stream,build the structure

--- calculator.cpp
+++ calculator.cpp
@@ -5,7 +5,94 @@
 
 using namespace std;
 
+//class
+class Token
+{
+  public:
+    char kind;
+    double value;
+    Token(char ch)
+        : kind(ch), value(0) {}
+    Token(char ch, double val)
+        : kind(ch), value(val) {}
+};
+
+class Token_stream
+{
+  public:
+    Token_stream();
+    Token get();
+    void putback(Token t);
+
+  private:
+    bool full;
+    Token buffer;
+};
+
+//Token_stream function
+Token_stream::Token_stream()
+    : full(false), buffer(0) {}
+
+void Token_stream::putback(Token t)
+{
+    buffer = t;
+    full = true;
+}
+
+Token Token_stream::get()
+{
+    if (full)
+    {
+        full = false;
+        return buffer;
+    }
+
+    char ch;
+    cin >> ch;
+
+    switch (ch)
+    {
+    case ';': //cout something
+    case 'q': //quit
+    case '(':
+    case ')':
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+        return Token(ch);
+    case '.':
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+    {
+        cin.putback(ch);
+        double val;
+        cin >> val;
+        return Token('8', val);
+    }
+    default:
+        cout << "Bad token";
+    }
+}
+
+//early declaration
+Token_stream ts;
+double expression();
+
+//rule
+double primary() {}
+double term() {}
+double expression() {}
+
+//main loop
 int main()
 {
-    
 }
